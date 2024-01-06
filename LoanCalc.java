0b668e921da31f02c0c929b4b351785962fc1ff3
lee dubrovsky <lee.dubrovsky@post.runi.ac.il>
2024-01-06 15:17:03 +0200

    third Hw

--- LoanCalc.java
+++ LoanCalc.java
@@ -63,7 +63,8 @@ public class LoanCalc {
         double H = loan ; 
         double g = (L + H) / 2;
         iterationCounter = 0;
-        while (Math.abs(endBalance(loan, rate, n, g)) > epsilon){
+
+        while (H - L > epsilon){
 
             if (endBalance(loan, rate, n, g) * endBalance(loan, rate, n, L) > 0){
                 L = g;             
